851f5d2b5636c9277e18dcb0533476003ccc9b86
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-20 14:32:31 +0100

    drop circles and contain them in a drop zone

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const app = new PIXI.Application({
 
 document.body.appendChild(app.view);
 
-// Create Matter.js engine
-const engine = Matter.Engine.create();
+// Create Matter.js engine with fixed time step and increased iterations
+const engine = Matter.Engine.create({
+  timing: {
+    timeScale: 1, // Adjust time scale as needed
+  },
+  positionIterations: 10, // Increase position iterations
+  velocityIterations: 10, // Increase velocity iterations
+});
 
 // Create Matter.js renderer
 const render = Matter.Render.create({
@@ -15,51 +21,83 @@ const render = Matter.Render.create({
   engine: engine,
 });
 
-// Set up Matter.js ground
+// Set up Matter.js container with walls
+const containerWidth = app.screen.width - 400;
+const containerHeight = app.screen.height - 50;
+
+const container = Matter.Composite.create();
+const leftWall = Matter.Bodies.rectangle(
+  200,
+  app.screen.height / 2,
+  10,
+  containerHeight,
+  { isStatic: true }
+);
+const rightWall = Matter.Bodies.rectangle(
+  app.screen.width - 200,
+  app.screen.height / 2,
+  10,
+  containerHeight,
+  { isStatic: true }
+);
 const ground = Matter.Bodies.rectangle(
   app.screen.width / 2,
   app.screen.height - 25,
   app.screen.width,
   50,
-  { isStatic: true }
+  { isStatic: true, restitution: 0.5 }
 );
-Matter.World.add(engine.world, ground);
+
+Matter.World.add(engine.world, [container, leftWall, rightWall, ground]);
+
+// Create PIXI.js graphics for walls and ground
+const groundGraphics = new PIXI.Graphics();
+groundGraphics.beginFill(0x996633);
+groundGraphics.drawRect(0, app.screen.height - 50, app.screen.width, 50);
+groundGraphics.endFill();
+app.stage.addChild(groundGraphics);
+
+const leftWallGraphics = new PIXI.Graphics();
+leftWallGraphics.beginFill(0x996633);
+leftWallGraphics.drawRect(200, 0, 10, app.screen.height);
+leftWallGraphics.endFill();
+app.stage.addChild(leftWallGraphics);
+
+const rightWallGraphics = new PIXI.Graphics();
+rightWallGraphics.beginFill(0x996633);
+rightWallGraphics.drawRect(app.screen.width - 210, 0, 10, app.screen.height);
+rightWallGraphics.endFill();
+app.stage.addChild(rightWallGraphics);
 
 // Run Matter.js engine and renderer
 Matter.Engine.run(engine);
 Matter.Render.run(render);
 
-const bunnies = [];
+const circles = [];
 
 document.body.onclick = () => {
-  const bunny = PIXI.Sprite.from("https://pixijs.com/assets/bunny.png");
-  bunny.anchor.set(0.5);
-  bunny.x = Math.random() * app.screen.width;
-  bunny.y = 0;
-  app.stage.addChild(bunny);
+  const radius = Math.floor(Math.random() * 100 + 20);
+  const circle = new PIXI.Graphics();
+  circle.beginFill(0xffffff);
+  circle.drawCircle(0, 0, radius);
+  circle.endFill();
+  circle.x = Math.random() * containerWidth + 200; // Position circles within the container
+  circle.y = 0;
+  app.stage.addChild(circle);
 
-  // Create Matter.js body for each bunny
-  const bunnyBody = Matter.Bodies.rectangle(
-    bunny.x,
-    bunny.y,
-    bunny.width,
-    bunny.height
-  );
-  Matter.World.add(engine.world, bunnyBody);
+  // Create Matter.js body for each circle
+  const circleBody = Matter.Bodies.circle(circle.x, circle.y, radius, {
+    restitution: 0.5,
+  });
+  Matter.World.add(engine.world, circleBody);
 
   const ticker = app.ticker.add(() => {
     // Update PIXI.js sprite position based on Matter.js body position
-    bunny.position.set(bunnyBody.position.x, bunnyBody.position.y);
+    circle.position.set(circleBody.position.x, circleBody.position.y);
 
-    // Apply gravity to the bunny
-    Matter.Body.applyForce(bunnyBody, bunnyBody.position, { x: 0, y: 0.002 });
-
-    // Check for collision with the ground
-    if (bunnyBody.position.y + bunny.height / 2 > app.screen.height - 25) {
-      // Apply an impulse to simulate a bounce
-      Matter.Body.applyForce(bunnyBody, bunnyBody.position, { x: 0, y: -0.02 });
-    }
+    // Apply gravity to the circle
+    Matter.Body.applyForce(circleBody, circleBody.position, { x: 0, y: 0.002 });
   });
 
-  bunnies.push({ bunny, ticker, body: bunnyBody });
+  circles.push({ circle, ticker, body: circleBody });
 };
