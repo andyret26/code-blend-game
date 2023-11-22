f07e5f528d02c8ce2ae0602cd5bf380528695aa5
andyret26 <andyret26@gmail.com>
2023-11-22 09:41:14 +0100

    Renaming

--- GetImgFromRadius.js
+++ GetImgFromRadius.js
@@ -1,13 +1,13 @@
 export function GetImgFromRadius(radius) {
-    if (radius === 20) return "./assets/orbs/html.png";
-    if (radius === 40) return "./assets/orbs/css.png";
-    if (radius === 60) return "./assets/orbs/js.png";
-    if (radius === 80) return "./assets/orbs/ts.png";
+    if (radius === 20) return "./assets/orbs/Html.png";
+    if (radius === 40) return "./assets/orbs/Css.png";
+    if (radius === 60) return "./assets/orbs/Js.png";
+    if (radius === 80) return "./assets/orbs/TS.png";
     if (radius === 100) return "./assets/orbs/ang.png";
-    if (radius === 120) return "./assets/orbs/react.png";
-    if (radius === 140) return "./assets/orbs/java.png";
+    if (radius === 120) return "./assets/orbs/React.png";
+    if (radius === 140) return "./assets/orbs/Java.png";
     if (radius === 160) return "./assets/orbs/net.png";
-    if (radius === 180) return "./assets/orbs/database 1.png";
-    if (radius === 200) return "./assets/orbs/azure.png";
-    if (radius === 120) return "./assets/orbs/experis.png";
+    if (radius === 180) return "./assets/orbs/Db.png";
+    if (radius === 200) return "./assets/orbs/AZ.png";
+    if (radius === 120) return "./assets/orbs/ex.png";
 }
